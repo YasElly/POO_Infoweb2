25fcf15c6f5d45b93dc5cb61b25da552dbb70569
YasElly <yef.brasil@gmail.com>
2026-08-11 17:14:53 +0000

    Iniciação a Streamlit

--- 2SEMESTRE/Aula_01/index.py
+++ 2SEMESTRE/Aula_01/index.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from retanguloUI import RetanguloUI
 
-st.header("POO em Python com Streamlit")
-st.write("Infoweb Turma 2026 - 2º Ano")
+# st.header("POO em Python com Streamlit")
+# st.write("Infoweb Turma 2026 - 2º Ano")
 
-if st.button("Clique Aqui"):
-    st.write("Bem vindo(a) ao Streamlit")
+# if st.button("Clique Aqui"):
+#     st.write("Bem vindo(a) ao Streamlit")
+
+RetanguloUI.main()
