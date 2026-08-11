ca97debece3d51f73e8711ce2133aa7f067d73d7
YasElly <yef.brasil@gmail.com>
2026-08-11 16:41:41 +0000

    MODIFICAÇÕES 2 SEMESTRE

---
+++ 2SEMESTRE/Aula_01/index.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header("POO em Python com Streamlit")
+st.write("Infoweb Turma 2026 - 2º Ano")
+
+if st.button("Clique Aqui"):
+    st.write("Bem vindo(a) ao Streamlit")
